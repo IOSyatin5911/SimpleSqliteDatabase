e05ec2f4f0e3e7599c81c35abda34a63baa21920
user11 <user11@user11s-Mac-mini.local>
2017-01-23 15:38:20 -0800

    Initial Commit

---
+++ demoSqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoSqlite
+//
+//  Created by user11 on 1/23/17.
+//  Copyright © 2017 user11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoSqlite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoSqlite
+//
+//  Created by user11 on 1/23/17.
+//  Copyright © 2017 user11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
